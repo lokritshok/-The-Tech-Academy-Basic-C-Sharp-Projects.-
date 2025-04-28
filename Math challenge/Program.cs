ae56f0d4b6d979899b46acb5163621a63b3221e4
victor shok <156413267+lokritshok@users.noreply.github.com>
2025-04-28 04:31:18 +0100

    Add project files.

---
+++ Math challenge/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Math_challenge
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Addition
+            //int num1 = 250;
+            //int num3 = 750;
+            //int total = num1 + num3;
+            //Console.WriteLine(total);
+
+            // Subtraction
+            //int number7 = 17;
+            //int number15 = 8;
+            //int difference = number7 - number15;
+            //Console.WriteLine(difference);
+
+            // Multiply
+            //int numbEight = 1000;
+            //int numbFive = 22;
+            //int product = numbEight * numbFive;
+            //Console.WriteLine(product);
+
+            // Divide
+            int N10 = 555;
+            int N2 = 5;
+            int quotient = N10/N2;
+            Console.WriteLine(quotient);
+        }
+    }
+}

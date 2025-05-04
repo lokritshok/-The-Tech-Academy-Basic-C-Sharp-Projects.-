0697e8b64df61decff551b47ec1f94535a428635
victor shok <156413267+lokritshok@users.noreply.github.com>
2025-05-04 23:36:02 +0100

    Add files via upload

---
+++ ACCOUNTING SYSTEM/ACCOUNTING SYSTEM/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace ACCOUNTING_SYSTEM
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("\n" + "Remember, we're \"accounting\" on you!");
+        }
+    }
+}
---
+++ ACCOUNTING SYSTEM/ACCOUNTING SYSTEM/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ACCOUNTING SYSTEM")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("ACCOUNTING SYSTEM")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("69844059-b881-48b1-ad1b-3af6de4ffb2b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

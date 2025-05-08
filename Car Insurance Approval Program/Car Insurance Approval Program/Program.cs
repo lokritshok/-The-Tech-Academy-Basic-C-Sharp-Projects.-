0dbdcffa0720741d7d5f957ea194caed7c289bb4
victor shok <156413267+lokritshok@users.noreply.github.com>
2025-05-08 03:45:01 +0100

    Add files via upload

---
+++ Car Insurance Approval Program/Car Insurance Approval Program/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Insurance_Approval_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              Console.WriteLine("Welcome to the Car Insurance Approval Program!");
+            Console.WriteLine("Please answer the following questions to determine your eligibility for car insurance.");
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many accidents have you had in the last 5 years?");
+            int accidents = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets have you received in the last 3 years?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+           
+            bool qualified = age > 15 && accidents == 0 && speedingTickets <= 3;
+            if (qualified)
+            {
+                Console.WriteLine("Congratulations! You are qualified for car insurance.");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you are not qualified for car insurance.");
+            }
+
+        }
+    }
+}
---
+++ Car Insurance Approval Program/Car Insurance Approval Program/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Car Insurance Approval Program")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Car Insurance Approval Program")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("af84d3b9-167e-44b6-a4ea-890f6539dec7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

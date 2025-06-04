fc95b5687a67d30a283f91678f29cc35aa05d4bb
victor shok <156413267+lokritshok@users.noreply.github.com>
2025-06-04 03:27:45 +0100

    Add files via upload

---
+++ Method Class Assignment Submission/Method Class Assignment Submission/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment_Submission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Store store = new Store();
+            ////store.Shoes = new List<Shoe>();
+
+            foreach (Shoe shoe in store.Shoes)
+            {
+                Console.WriteLine(shoe.Color + " of size " + shoe.Size);
+            }
+            //sum of shoes in store
+            Console.WriteLine(store.Shoes.Count);
+
+
+            //// Shoe shoeOne = new Shoe();
+            //// shoeOne.Size = "9";
+            ////shoeOne.Color = "Red";
+
+            ////store.Shoes.Add(shoeOne);
+
+            ////Console.WriteLine(shoeOne.Color + " of size: " + shoeOne.Size);
+            ////Console.WriteLine(store.Shoes[0].Color + " of size " + store.Shoes[0].Size);
+            Console.ReadLine();
+        }
+    }
+}

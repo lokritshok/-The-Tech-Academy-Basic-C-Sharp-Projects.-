bab35b65c2b0de792f06e5f368ad9b20c1629036
victor shok <156413267+lokritshok@users.noreply.github.com>
2025-06-24 01:14:44 +0100

    Add files via upload

---
+++ Struct Assignment/Struct Assignment/Number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    struct Number
+    {
+        // Properties
+        public decimal Amount { get; set; }
+        //convert to decimal
+        // Constructor
+        public Number(decimal amount)
+        {
+            Amount = amount;
+        }
+
+
+    }
+}
---
+++ Struct Assignment/Struct Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //creating an instance of Number struct
+            Number number = new Number(1999.9m);
+            
+          //displaying the value of Amount property
+            Console.WriteLine("The amount is: " + number.Amount);
+            Console.ReadLine();
+        }
+    }
+}

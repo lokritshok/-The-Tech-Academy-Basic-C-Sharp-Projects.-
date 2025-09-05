3b8b4ba55297284bd805ea4223d7223e3b7cabab
victor shok <156413267+lokritshok@users.noreply.github.com>
2025-09-05 10:24:57 +0100

    Add files via upload

---
+++ StudentApps/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace StudentApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDBContext db = new AppDBContext();
+            db.Students.Add(new Student() { Name = "Victor" });
+            db.SaveChanges();
+            Console.WriteLine("Data Saved");
+        }
+    }
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    class AppDBContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
+
